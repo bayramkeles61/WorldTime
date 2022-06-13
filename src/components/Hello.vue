2fb3270e5ba012be446ae30f9de335bf407d398c
bayramkeles61 <79147272+bayramkeles61@users.noreply.github.com>
2022-06-13 10:39:08 +0300

    feat: setup components

---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+	defineProps<{
+		name: string;
+	}>();
+
+	//! DATA
+
+	//! METHODS
+
+	//! COMPUTED
+
+	//! CREATED
+</script>
+
+<template>
+	<div>Hello, {{ name }}</div>
+</template>
