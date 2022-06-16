791ce2842451b7adee6189521638c1b4448fc398
bayramkeles61 <79147272+bayramkeles61@users.noreply.github.com>
2022-06-16 17:48:11 +0300

    feat: display time for timezone

--- src/components/TimezoneItem.vue
+++ src/components/TimezoneItem.vue
@@ -5,11 +5,22 @@
 		timezone: Timezone;
 	}>();
 
-	const state = $computed(() => timezone.name.split("/")[0]);
-	const city = $computed(() => timezone.name.split("/")[1]);
+	const formatter = new Intl.DateTimeFormat("en-US", {
+		timeZone: timezone.name,
+		hour12: false,
+		hour: "numeric",
+		minute: "numeric",
+	});
+	const state = $computed(() =>
+		timezone.name.split("/")[0].replace(/_/g, "")
+	);
+	const city = $computed(
+		() => timezone.name.split("/")[1].replace(/_/g, "") || ""
+	);
 	const offset = $computed(() =>
 		timezone.offset > 0 ? `+${timezone.offset}` : `-${timezone.offset}`
 	);
+	const time = $computed(() => formatter.format(now.value));
 </script>
 
 <template>
@@ -23,12 +34,12 @@
 		<div flex="~col" text-left flex-auto w-30 ws-nowrap>
 			<div>
 				<span of-hidden text-ellipsis mr-1>{{ city }}</span>
-				<!-- <sup border="~ base rounded" px1>{{ timezone.abbr }}</sup> -->
+				<sup border="~ base rounded" px1>{{ timezone.abbr }}</sup>
 			</div>
 			<div text-sm op50 leading-1em>{{ state }}</div>
 		</div>
-		<!-- <div tabular-nums ma text-sm w-20 text-right>
-    {{ time }}
-    </div> -->
+		<div tabular-nums ma text-sm w-20 text-right>
+			{{ time }}
+		</div>
 	</div>
 </template>
